d9c1b68ed7dbf94a5886e2a9f51f68bf822d6925
leeheejo <jo4610jo@naver.com>
2021-06-23 16:07:19 +0900

    [210623] 스칼라 for문

---
+++ src/Ex3_3.scala
@@ -0,0 +1,8 @@
+
+
+object Ex3_3 {
+  def main(args: Array[String]): Unit = {
+    for (x <- 1 to 10) 
+      println("x의 값은 " + x) // 10까지 출력 된다 
+  }
+}
---
+++ src/Ex3_4.scala
@@ -0,0 +1,8 @@
+
+
+object Ex3_4 {
+  def main(args: Array[String]): Unit = {
+    for (x <- 1 until 10)
+      println("x의 값은 " + x) // 9까지 출력 된다
+  }
+}
