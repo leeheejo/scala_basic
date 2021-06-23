bbdfc8bb242e1981abab09af884b348a3de9bd2a
leeheejo <jo4610jo@naver.com>
2021-06-23 15:50:24 +0900

    [210623] 스칼라 if문

---
+++ src/Ex3_1.scala
@@ -0,0 +1,14 @@
+
+
+object Ex3_1 {
+  def main(args: Array[String]): Unit = {
+    var x = 20
+    if (x > 15) {
+      println("15보다 큽니다 ")
+    } else if (x < 15) {
+      println("15보다 작습니다 ")
+    } else {
+      println("과연 뭘까요?")
+    }
+  }
+}
---
+++ src/Ex3_2.scala
@@ -0,0 +1,8 @@
+
+
+object Ex3_2 {
+  def main(args: Array[String]): Unit = {
+    var x = 20
+    if (x > 15) println("껄껄껄")
+  }
+}
